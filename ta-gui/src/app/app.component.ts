952678a429986cab9b25afcd414bbeb703193b71
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:03:47 -0300

    elementos do formulario para cadastro de alunos

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nova-gui';
+
+   aluno: Aluno = {nome: "", cpf: "", email: ""};
+}
+
+export class Aluno {
+  nome: string;
+  cpf: string;
+  email: string;
 }
